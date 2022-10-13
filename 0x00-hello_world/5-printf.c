7d21f3cf010fc209f9519f4e08a19ee5f281f7d0
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-13 16:33:13 -0700

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descrition: Print a string with prinf
+ *
+ * Return: Always 0(Success)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
