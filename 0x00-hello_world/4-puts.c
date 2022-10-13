eb56c19605928ae0f4a83cbe2583dcdc5d861226
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-13 16:24:08 -0700

    Basic puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: 'Print a string with puts()'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
