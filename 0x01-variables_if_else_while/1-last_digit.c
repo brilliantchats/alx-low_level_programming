8daf971aa2b1cd48ef6903aafae8e5e751124556
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-14 15:10:27 -0700

    more control flow

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main -Entry point
+ *
+ * Description: if-else once more
+ *
+ * Return: 0(Success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	/* Last digit of a number */
+	int last;
+	last = n % 10;
+
+	if (last > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n last);
+	} else if (last < 6 && last != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not zero", n last);
+	} else
+	{
+		printf("Last digit of %d is %d and is zero", n last);
+	}
+}
