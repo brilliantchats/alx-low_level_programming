cbc775b86a4cf67d89cb7f87897785c3202c94fa
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-14 18:16:31 -0700

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print alhabet characters
+ *
+ * Return: 0(Success)
+ *
+ */
+int main(void)
+{
+	char 'i';
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	retunr (0);
+}
