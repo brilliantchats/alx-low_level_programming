c84d5f52ec0c4477b996ccaa034d669c6a6846fd
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-16 03:39:37 -0700

    Print hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print hexadecimal numbers
+ *
+ * Return: (0)Success
+ *
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+	return (0);
+}
