ae2f636e24a1ae3635874f9372326572d1e9f8d7
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-16 02:45:42 -0700

    Print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print numbers
+ *
+ * Return: (0)Success
+ *
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+	while (i <10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	printf("\n");
+	return (0);
+}
