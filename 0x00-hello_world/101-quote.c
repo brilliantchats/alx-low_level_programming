29c59810341a847476961ed2209ed18489b64a7d
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-17 03:59:22 -0700

    print without stdio library

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 
-int main()
+/**
+ * main -Entry Point
+ *
+ * Description:Print without using printf or puts
+ *
+ * Return: 1(Success)
+ *
+ */
+
+int main(void)
 {
 	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
 	return (1);
