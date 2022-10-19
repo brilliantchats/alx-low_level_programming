6b6749fa5f749899d9ea5e50431373b185497ea2
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-18 19:24:05 -0700

    Print without printf, puts, etc

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print out a string without standard Lib
+ *
+ * Return: 0(Success)
+ *
+ */
+int main(void)
+{
+	system("echo '_putchar\n'");
+	return (0);
+}
