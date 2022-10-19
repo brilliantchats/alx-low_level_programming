cf2899de68970ee987c6bf58c0ac5158e302a82c
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-19 12:35:24 -0700

    Print without the standard library

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - Entry Point
@@ -10,6 +10,14 @@
  */
 int main(void)
 {
-	system("echo '_putchar\n'");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER-FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+
+#endif
