12b2e6df511499247848357564c289a19d5c2045
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-19 16:28:06 -0700

    multiplication tabl

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,28 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if ( j == 9)
+			if (j == 9)
 			{
-				_putchar((i * j) + '0');
+				if ((i * j) >= 10)
+				{
+					_putchar(((i * j) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
+				} else
+				{
+					_putchar((i * j) + '0');
+				}
 				_putchar('$');
 				_putchar('\n');
 			} else
 			{
-				_putchar((i * j) + '0');
+				if ((i * j) >= 10)
+				{
+					_putchar(((i * j) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
+				} else
+				{
+					_putchar((i * j) + '0');
+				}
 				_putchar(',');
 				_putchar(' ');
 			}
