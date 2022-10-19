379f6402184508dc4f51fd8930a8c9e5a04384fe
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-19 15:51:02 -0700

    Print time in 24 hr format

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print time
+ *
+ * Description - Print time in a 24 hr format
+ *
+ * Return: void(Success)
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (m = 0; m < 10; m++)
+				{
+					if (i == 2 && j > 3)
+					{
+						break;
+					}
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(m + '0');
+				}
+			}
+		}
+	}
+}
