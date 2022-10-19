080589aec1b14390cd1fcee5096e47ee3f8fe891
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-19 13:40:56 -0700

    Check to see if argument is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry Point
+ *
+ * Description: Check whether input is a lowercase
+ *
+ * Return: 1(Success), 0(Fail)
+ *
+ */
+int _islower(int c)
+{
+	int i = c;
+	int j;
+
+	for (j = 97; j <= 122; j++)
+	{
+		if (i == j)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
