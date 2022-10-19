f961d320f71d449c387ee3001f6140a53d57bacd
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-19 14:52:40 -0700

    Return absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Print absolute number
+ * @x: number passed
+ *
+ * Description: Output the absolute number from x
+ *
+ * Return: absolute number(Success)
+ *
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	} else if (x < 0)
+	{
+		return (x * -1)
+	}
+}
