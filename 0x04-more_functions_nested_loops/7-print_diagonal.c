17433ef1230bee0b6e4fa8f1e94fc73a899b3e67
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-20 10:23:51 -0700

    Print a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print a diagonal line
+ * @n: number of / to be printed
+ *
+ * Description: Printa diagonal line using /
+ *
+ * Return: void
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('/');
+		_putchar('\n');
+	}
+}
