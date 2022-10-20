7d41b215fbf42df58ee52bec7a11e0f71e7bcd87
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-20 05:13:12 -0700

    Multiply two given numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multily 2 numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Description: Multiply two numbers
+ *
+ * Return: The product of the multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
