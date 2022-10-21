cdb90d64a3e6ea93b5448b5e05384d5d3f53991d
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-21 06:41:28 -0700

    Testing the positive_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -tests function which determines if num is +,- or 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+
+	return (0);
+}
