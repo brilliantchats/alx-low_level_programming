f07b02557c8d4972d7f7b2fe42dc286d72d2fee1
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-10-31 08:59:18 -0700

    fill up s with b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _memset - Fills up a given memory
+ * *_memset - Fills up a given memory
  * @s: String to be filled
  * @b: The byte to fill up the string
  * @n: The number of byte to be printed with b
+ *
+ * Description: Fill up memory pointed to by s with b
  *
  * Return: *s (Pointer to the string)
  *
