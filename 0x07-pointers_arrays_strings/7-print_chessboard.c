41b3d392b98e8b0ad3702c3fe420390d4f612a91
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-01 04:51:59 -0700

    Print a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a 2D array
+ * @a: the array
+ *
+ * Return: void
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j] + '0');
+		}
+		_putchar('\n');
+	}
+}
