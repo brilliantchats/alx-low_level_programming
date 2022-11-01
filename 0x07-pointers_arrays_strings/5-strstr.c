ec044e9202b9ff68083c13c13e7da5c4d3f34d38
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-01 02:22:53 -0700

    Find a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strstr - Find a substring
+ * @haystack: the string
+ * @needle: the substring to search for
+ *
+ * Return:pointer to the first char of the found substrng or null
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k, pointer, found;
+
+	found = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; j < 1; j++)
+		{
+			if (needle[j] != haystack[i])
+				continue;
+			else
+			{
+				found += 1;
+				for (k = 0; needle[k] != '\0'; k++)
+				{
+					if (needle[k] != haystack[i + k])
+						found -= 1;
+						break;
+				}
+				if (found)
+				{
+					pointer = i;
+					return (haystack + i);
+				}
+			}
+		}
+	}
+	return ('\0');
+}
