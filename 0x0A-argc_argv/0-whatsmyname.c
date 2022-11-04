2dc3e2c2bfdb0f8dc83bacab51c1963344524813
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-04 03:49:22 -0700

    Use argv to print out the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: counter of arguments
+ * @argv: array of parameters
+ *
+ * Return: 0(Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argv[0][i] != '\0'; i++)
+	{
+		_putchar(argv[0][i]);
+	}
+	return (0);
+}
