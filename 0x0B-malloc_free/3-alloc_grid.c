602bc11b436e7041d19eb4d52a34611110971f64
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-09 12:07:00 -0800

    2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,7 +30,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			array[i][j] = 0;
+		{
+			*(*(arr + i) + j) = 0;
+		}
 	}
 	return (array);
 }
