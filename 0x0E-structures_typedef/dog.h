80722990c234d36e967d761d9ab72499ef635f39
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-14 22:23:18 +0000

    Defining a simple struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new struct named dog
+ * @name: Name of the Dog object
+ * @age: Age of the object
+ * @owner: Owner of the object
+ *
+ * Description: Struct defining a new type dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
