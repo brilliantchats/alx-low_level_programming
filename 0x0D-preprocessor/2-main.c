9c8565d3f07395e8812c8269afe05294fbfb70e0
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-14 20:17:45 +0000

    Print out the file name using predefined macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints out the name of the file 
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int i;
+	char *str;
+
+	str = __FILE__;
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
