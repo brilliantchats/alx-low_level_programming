5b6776e6b977645fa7072ef018c858521bd312e2
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-15 19:45:56 +0000

    Making to run struct code if struct dog is not null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,10 @@ void print_dog(struct dog *d)
 {
 	char *nil = "(nil)";
 
-	printf("Name: %s\n", (d->name == NULL ? nil : d->name));
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner == NULL ? nil : d->owner));
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name == NULL ? nil : d->name));
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner == NULL ? nil : d->owner));
+	}
 }
