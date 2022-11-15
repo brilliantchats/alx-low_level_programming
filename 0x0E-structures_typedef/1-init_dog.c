7774d0704c7cec98e32b0e098eb33aa7820f6f20
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-15 18:52:48 +0000

    Initialising a struct through a function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
