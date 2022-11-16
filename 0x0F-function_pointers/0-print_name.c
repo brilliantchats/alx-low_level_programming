7b5eae5c7ad3006d936b55a2a738992c503993a1
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-16 13:31:56 +0000

    Print out a string via a function passed as an argument

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - Prints a string using a function passed
+ * @name: the string
+ * @f: the function passed to print out the string
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
