275fd3c7f9c64cca4da64623156115f7de011840
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-11-29 17:11:24 +0000

    Free a list and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint2 - Free a list and set head to NULL
+ * @head: the first element of the list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = head;
+	listint_t *temp;
+
+	while (current != NULL)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
+	head = NULL;
+}
