e694ea68c2508cefd1f98edeaf8fa1509341f23b
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-12-05 16:18:42 +0000

    Read a text file and print it to the standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - Reads and prints a text file provided
+ * @filename: Name of the text file
+ * @letters: the number of characters to be read and printed
+ *
+ * Return: the actual number of characters printed, 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t written, read_num;
+	int fd;
+	char *buffer = malloc(sizeof(char) * letters);
+
+	if ((buffer == NULL) | (filename == NULL) | (letters == 0))
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	read_num = read(fd, buffer, letters);
+	if (read_num == -1)
+		return (0);
+	written = write(STDOUT_FILENO, buffer, read_num);
+	if (written == -1)
+		return (0);
+	return (written);
+}
