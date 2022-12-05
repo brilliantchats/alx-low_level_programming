fb2559b767b01d9d24b95fb0fbf2425415c339b9
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-12-05 18:45:45 +0000

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,10 +31,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (open(filename, O_RDONLY))
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
-	else
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
