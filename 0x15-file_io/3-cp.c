3d3f2a3ffa609fcc1011191d76838355a13b6574
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2022-12-06 14:44:18 +0000

    A program that copies one file's contents into another file's content

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,98 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFFER 1024
+/**
+ * read_write - read from one file and write in another
+ * @read_desc: the file descriptor for read
+ * @wrt_desc: the file descriptor for write
+ * @buffr: the character buffer to read and write to
+ * @frm: file to be copied
+ * @to: destination file
+ *
+ * Return: nothing
+ */
+void read_write(int read_desc, int wrt_desc, char *buffr, char *frm, char *to)
+{
+	ssize_t read_num, written;
+
+	while (1)
+	{
+		read_num = read(read_desc, buffr, BUFFER);
+		if (read_num == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s", frm);
+			exit(98);
+		}
+		if (read_num == BUFFER)
+		{
+			written = write(wrt_desc, buffr, read_num);
+			if (written == -1)
+			{
+				dprintf(STDERR_FILENO,
+					"Error: Can't write to %s", to);
+				exit(99);
+			}
+			free(buffr);
+		}
+		else if (read_num < BUFFER)
+		{
+			written = write(wrt_desc, buffr, read_num);
+			if (written == -1)
+			{
+				dprintf(STDERR_FILENO,
+					"Error: Can't write to %s", to);
+				exit(99);
+			}
+			free(buffr);
+			break;
+		}
+		buffr = malloc(BUFFER);
+	}
+}
+/**
+ * main - Main function
+ * @argc: the number of arguments on the command line
+ * @argv: character array of arguments on the command line
+ *
+ * Return: 0 for Success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_read, fd_write, closed;
+	char *buf = malloc(BUFFER);
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	fd_read = open(argv[1], O_RDONLY);
+	if (fd_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s",
+				argv[1]);
+		exit(98);
+	}
+	fd_write = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_write == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		exit(99);
+	}
+	read_write(fd_read, fd_write, buf, argv[1], argv[2]);
+	closed = close(fd_read);
+	if (closed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_read);
+		exit(100);
+	}
+	closed = close(fd_write);
+	if (closed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_write);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 size_t str_len(char *str);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void read_write(int read_desc, int wrt_desc, char *buffr, char *frm, char *to);
 #endif /* MAIN_H */
