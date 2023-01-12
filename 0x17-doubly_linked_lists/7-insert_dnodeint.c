e7b221abb9697de1300e63a569911febbe29d8a2
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-12 13:27:35 +0000

    A C script inserts a new node at a given position. 2-add_dnodeint.c and 3-add_dnodeint_end.c will be compiled together with this script in order to use functions in those scripts

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_dnodeint_at_index - Insert a node at a given index
+ * @h: the first element of the list
+ * @idx: the index to insert the node
+ * @n: the integer value of the new node to be inserted
+ *
+ * Return: address of the new node or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h, *previous, *new;
+	unsigned int i = 0;
+
+	if (*h == NULL)
+		return (NULL);
+	while (current != NULL)
+	{
+		if (idx == i)
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+				return (NULL);
+			if (idx == 0)
+			{
+				new = add_dnodeint(&current, n);
+				return (new);
+			}
+			if (current->next == NULL)
+			{
+				new = add_dnodeint_end(&current, n);
+				return (new);
+			}
+			new->n = n;
+			new->prev = previous;
+			new->next = current;
+			previous->next = new;
+			current->prev = new;
+			return (new);
+		}
+		previous = current;
+		current = current->next;
+		i++;
+	}
+	return (NULL);
+}
