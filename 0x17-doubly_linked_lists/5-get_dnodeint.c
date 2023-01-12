4b547921c9df1697a72be0049f89dc35d1021159
Brilliant Chatuwa <brilliantchatuwa182@gmail.com>
2023-01-12 12:46:41 +0000

    Fixing a betty style issue

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (current != NULL)
 	{
 		if (index == i)
-			return current;
+			return (current);
 		current = current->next;
 		i++;
 	}
